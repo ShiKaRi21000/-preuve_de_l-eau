3198d42a6423f996e461851995a8a772dfdc1d87
GHERBI Kévin <kgherbi21@gmail.com>
2023-12-18 20:00:27 +0100

    modif de eau01.js

--- eau01.js
+++ eau01.js
@@ -3,7 +3,12 @@ function nombre_1 (){
         for(let j = 0; j < 10; j++){
             for(let k = 0; k < 10; k++){
                 for(let l = 0; l < 10; l++){
-                    console.log(`${i}${j}` + " " + `${k}${l}`)
+                    if ((`${i}${j}` === `${k}${l}`)){
+
+                        }
+                    else {
+                        console.log(`${i}${j}` + " " + `${k}${l}`)
+                    }
                 }
             }
         }
