4e1111adfbfec5abe52e74865158808bcb19508f
GHERBI Kévin <kgherbi21@gmail.com>
2023-12-18 17:43:25 +0100

    Ajout de eau00.js

--- eau00.js
+++ eau00.js
@@ -1,1 +1,15 @@
-console.log("débtu de l'eau")
+function combinaison (){
+    for (let i = 0; i < 10; i++){
+        for (let j = 0; j < 10; j++){
+            for (let k = 0; k < 10; k++){
+                if ((i === k) || (i === j) || (j === k)){
+                }
+                else{
+                    console.log(i, j, k)
+                }
+            }
+        } 
+    }
+}
+
+combinaison()
