0ffdbebb1b1abbd72c7bc9b785444ca6f151a858
GHERBI Kévin <kgherbi21@gmail.com>
2023-12-29 10:46:41 +0100

    Ajout de eau04.js

---
+++ eau04.js
@@ -0,0 +1,28 @@
+
+function isPrime(n) {
+    
+    if (n <= 1) return false
+
+    for (var i = 2; i <= n - 1; i++)
+      if (n % i == 0) return false
+
+    return true
+}
+
+
+function nbrPremier (nbr){
+
+    if ((nbr === '') || isNaN(nbr) || (Math.sign(nbr) === -1)){
+        console.log( -1)
+    }
+    else {
+        for (var i = nbr; i < (nbr * 4); i++)
+            if (isPrime(i)){
+                return console.log(i)
+        }
+    }
+}
+isPrime()
+nbrPremier(process.argv[2])
+
+
