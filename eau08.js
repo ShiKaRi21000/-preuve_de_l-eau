d7551d1be77fcecbbb3502d6a443503852be3616
GHERBI Kévin <kgherbi21@gmail.com>
2024-01-29 15:29:37 +0100

    Ajout de eau08.js

--- eau08.js
+++ eau08.js
@@ -1,17 +1,25 @@
 
 function queDesChiffres (chaineTester) {
+        let chiffre = ""
 
-    let chiffre = ""
-
-    for (i of chaineTester){
-        if (Number.isInteger(parseInt(i))){
-            chiffre += i
+        for (i of chaineTester){
+            if (Number.isInteger(parseInt(i))){
+                chiffre += i
+            }
+            else {
+                console.log("Error: la chaine de caractère ne contient pas que des chiffres")
+                break
+            }
         }
-        else {
-            console.log("Error: la chaine de caractère ne contient pas que des chiffres")
-            break
+        if (chiffre === chaineTester) {
+            console.log ("La chaine de caractère ne contient que des chiffres.")
         }
     }
+
+if (process.argv[2] === ""){
+    console.log("Error: la chaine de caractère est vide")
 }
 
-queDesChiffres (process.argv[2])
+else {
+    queDesChiffres (process.argv[2])
+}
