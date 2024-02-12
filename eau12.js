58789fc0092d635cf00cdaac3c6657759daab313
GHERBI Kévin <kgherbi21@gmail.com>
2024-02-12 17:04:24 +0100

    modif de eau12.js

--- eau12.js
+++ eau12.js
@@ -17,15 +17,14 @@ function getValidArguments (arguments) {
 }
 
 function my_bubble_sort(arr){
-    const n = arr.length;
-    for (let i = 0; i < n - 1; i++) {
-        for (let j = 0; j < n - i - 1; j++) {
+    for (let i = 0; i < arr.length; i++) { 
+        for (let j = 0; j < (arr.length - i - 1); j++) {
             if (arr[j] > arr[j + 1]) {
-                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
             }
         }
     }
-    return arr;
+    console.log(arr)
 }
 
 function resultat(){
